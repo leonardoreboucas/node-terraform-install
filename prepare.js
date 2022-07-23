a09d8d51ba93a5340070558790e32eeb4eada56c
Leonardo Rebouças de Carvalho <leonardo_r_carvalho@yahoo.com.br>
2022-07-22 21:43:31 -0300

    Removind root check

--- prepare.js
+++ prepare.js
@@ -1,11 +1,11 @@
-const fs = require('fs');
-const os = require('os');
-console.log(os.homedir())
-const filePath = os.homedir()+'/.terraform/terraform'; 
-try {
+//const fs = require('fs');
+//const os = require('os');
+//console.log(os.homedir())
+//const filePath = os.homedir()+'/.terraform/terraform'; 
+/*try {
   if (fs.existsSync(filePath)) {
     fs.unlinkSync(filePath);
   }
 } catch(err) {
   console.error(err)
-}
+}*/
